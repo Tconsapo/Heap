a6f53b54468cd11e6af568fa9b50ee3afe3330ed
Tcoon <>
2016-04-13 09:57:18 +0300

    Heap + correctTests

--- src/test/java/com/my/heap/HeapTest.java
+++ src/test/java/com/my/heap/HeapTest.java
@@ -6,12 +6,6 @@
 package com.my.heap;
 
 import static com.my.heap.Main.init;
-import java.util.Arrays;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
